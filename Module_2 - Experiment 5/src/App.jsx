e463bb82aa323a1afe0eb626cc9a78db55aab1a5
geekyhim <work.himanshu1720@gmail.com>
2025-10-18 16:30:17 +0530

    Module 2 Experiment 5

---
+++ Module_2 - Experiment 5/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import * as ReactDOM from "react-dom";
+import { 
+  createBrowserRouter,
+   RouterProvider
+} from "react-router-dom";
+import Dashboard from './compenents/Dashboard';
+import About from './compenents/About';
+import Home from './compenents/home';
+import Login from './compenents/Login';
+import SignUp from './compenents/SignUp';
+import Navbar from './compenents/Navbar';
+import AdminDashboard from './compenents/Admin/AdminDashboard';
+
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element: <div>
+        <Navbar/>
+        <Home />
+      </div>
+    },
+    {
+      path:"/about",
+      element:<About />
+    },
+    {
+      path:"/login",
+      element: <Login />
+    },
+    {
+      path:"/signup",
+      element: <SignUp />
+    },
+    {
+      path:"/dashboard",
+      element:<Dashboard/>
+    },
+    {
+      path:"/admin",
+      element:<AdminDashboard/>
+    }
+  ]
+)
+
+
+
+
+function App() {
+
+  return (
+    <>
+       <RouterProvider router={router} />
+
+    </>
+  )
+}
+
+export default App
